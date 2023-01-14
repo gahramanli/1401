b0550974b9c5801f2c331b508c70c1a787605de9
gahramanli <116644249+gahramanli@users.noreply.github.com>
2023-01-14 22:25:07 +0400

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+import './App.css';
+import Page from './Components/page.js';
+
+
+function App() {
+  return(
+      <Page/> 
+  );
+  
+}
+export default App; 
---
+++ src/Components/page.js
@@ -0,0 +1,165 @@
+import React from 'react'
+import   './page.css';
+
+const Page = () => {
+    return(
+        <header>
+            <div class="navbar">
+                <div class="logo">Start Bootstrap</div>
+                <div class="list">
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li>Pricing</li>
+                        <li>FAQ</li>
+                        <li>Blog</li>
+                        <li>Portfolio</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="second">
+                <div class="f1rst">
+                    <h1>A Bootstrap 5<br></br> template for<br></br> modern businesses</h1>
+                    <p>Quickly design and customize responsive mobile-<br></br>first sites with Bootstrap, the world’s most popular<br></br> front-end open source toolkit!</p>
+                    <div class="buttons">
+                        <button class="btn1">Get Started</button>
+                        <button class="btn2">Learn More</button>
+                    </div>
+                </div>
+                <div class="s2cond">
+                    <img src="https://dummyimage.com/600x400/343a40/6c757d"></img>
+                </div>
+            </div>
+            <div class="third">
+                <div class="f11rst">A better way to<br></br> start building.</div>
+                <div class="s22cond">
+                    <img src="https://www.elgato.com/themes/custom/smalcode/key-creator/assets/image_pool/sd3/btn_egc_select_scene.svg"></img>
+                    <h2>Featured title</h2>
+                    <p>Paragraph of text beneath the heading<br></br>to explain the heading. Here is just a bit<br></br> more text.</p>
+                </div>
+                <div class="s22cond">
+                    <img src="https://www.elgato.com/themes/custom/smalcode/key-creator/assets/image_pool/sd3/btn_egc_select_scene.svg"></img>
+                    <h2>Featured title</h2>
+                    <p>Paragraph of text beneath the heading<br></br>to explain the heading. Here is just a bit<br></br> more text.</p>
+
+                </div>
+            </div>
+                <div class="third">
+                    <div class="f11rst"></div>
+                    <div class="s22cond">
+                        <img src="https://www.elgato.com/themes/custom/smalcode/key-creator/assets/image_pool/sd3/btn_egc_select_scene.svg"></img>
+                        <h2>Featured title</h2>
+                        <p>Paragraph of text beneath the heading<br></br>to explain the heading. Here is just a bit<br></br> more text.</p>
+                    </div>
+                    <div class="s22cond">
+                        <img src="https://www.elgato.com/themes/custom/smalcode/key-creator/assets/image_pool/sd3/btn_egc_select_scene.svg"></img>
+                        <h2>Featured title</h2>
+                        <p>Paragraph of text beneath the heading<br></br>to explain the heading. Here is just a bit<br></br> more text.</p>
+                    </div>
+
+                </div>
+                <div class="fourth">
+                    <p><em>"Working with Start Bootstrap templates has saved<br></br> me tons of development time when building new<br></br> projects! Starting with a Bootstrap template just<br></br> makes things easier!"</em></p>
+                    <div class="bottom">
+                        <img src="https://dummyimage.com/40x40/ced4da/6c757d"></img>
+                        <p>Tom Ato / CEO, Pomodoro</p>
+                    </div>
+                </div>
+                <div class="fifth">
+                    <h4>From our blog</h4>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing<br></br> elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
+                </div>
+                <div class="sixth">
+                    <div class="box-1">
+                        <div class="image">
+                            <img src="https://www.system-concepts.com/wp-content/uploads/2019/06/jigsaw-concept-600x350px.jpg   "></img>
+                        </div>
+                        <div class="fav-icon">
+                            <img src="https://www.amansw.com.au/wp-content/uploads/2019/03/Article-images-March_April-20199.jpg"></img>
+                        </div>
+                        <div class="card-body">
+                            <h5>Blog post title</h5>
+                            <p>Some quick example text to build<br></br> on the card title and make up the<br></br> bulk of the card's content.</p>
+                        </div>
+                        <div class="cardfooter">
+                            <img src="https://dummyimage.com/40x40/ced4da/6c757d"></img>
+                            <div class="footer-foot">
+                                <h6>Kelly Rowan</h6>
+                                <p>Marc 12, 2022 - 6 min read</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-1">
+                        <div class="image">
+                            <img src="https://www.system-concepts.com/wp-content/uploads/2019/06/jigsaw-concept-600x350px.jpg   "></img>
+                        </div>
+                        <div class="fav-icon">
+                            <img src="https://www.amansw.com.au/wp-content/uploads/2019/03/Article-images-March_April-20199.jpg"></img>
+                        </div>
+                        <div class="card-body">
+                            <h5>Blog post title</h5>
+                            <p>Some quick example text to build<br></br> on the card title and make up the<br></br> bulk of the card's content.</p>
+                        </div>
+                        <div class="cardfooter">
+                            <img src="https://dummyimage.com/40x40/ced4da/6c757d"></img>
+                            <div class="footer-foot">
+                                <h6>Kelly Rowan</h6>
+                                <p>Marc 12, 2022 - 6 min read</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-1">
+                        <div class="image">
+                            <img src="https://www.system-concepts.com/wp-content/uploads/2019/06/jigsaw-concept-600x350px.jpg   "></img>
+                        </div>
+                        <div class="fav-icon">
+                            <img src="https://www.amansw.com.au/wp-content/uploads/2019/03/Article-images-March_April-20199.jpg"></img>
+                        </div>
+                        <div class="card-body">
+                            <h5>Blog post title</h5>
+                            <p>Some quick example text to build<br></br> on the card title and make up the<br></br> bulk of the card's content.</p>
+                        </div>
+                        <div class="cardfooter">
+                            <img src="https://dummyimage.com/40x40/ced4da/6c757d"></img>
+                            <div class="footer-foot">
+                                <h6>Kelly Rowan</h6>
+                                <p>Marc 12, 2022 - 6 min read</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="seventh">
+                    <div class="i-card">
+                        <div class="frst">
+                            <h7>New products, delivered to you.</h7>
+                            <p>Sign up for our newsletter for the latest updates.</p>
+                        </div>
+                        <div class="scnd">
+                            <div class="form">
+                                <input class="my1" placeholder="Email address..."></input>
+                                <button class="btn3">Sign Up</button>
+                            </div>
+                            <p>We care about privacy, and will never share your data.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer">
+                    <p>
+                    Copyright © Your Website 2022
+                    </p>
+                    <ul>
+                        <li>Privacy</li>
+                        <li>Terms</li>
+                        <li>Contact</li>
+
+                    </ul>
+                </div>
+            
+
+                
+                
+        </header>  
+    )
+}
+export default Page
